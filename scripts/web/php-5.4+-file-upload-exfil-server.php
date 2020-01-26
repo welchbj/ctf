280a8f781c91e9c4d0f97b7d43a5780a9df93708
Brian Welch <welch18@vt.edu>
2020-01-26 17:19:44 -0500

    Minor tweaks to PHP HTTP exfil server

--- scripts/web/php-5.4+-file-upload-exfil-server.php
+++ scripts/web/php-5.4+-file-upload-exfil-server.php
@@ -1,40 +1,54 @@
 <?php
 /**
+ * IMPORTANT NOTE:
+ * This server is wildly insecure and should only be used for one-off file
+ * transfers to yourself.
+ *
  * Invoke this with:
- * php -S localhost:8888 php-5.4+-file-upload-exfil-server.php
+ * mv php-5.4+-file-upload-exfil-server.php index.php
+ * php -S 0.0.0.0:8888
  */
 
 define('FILE_FORM_FIELD', 'f');
 define('MAX_FILE_SIZE', 1000000);
+define('UPLOAD_DIR', './uploads');
 
 header('Content-Type: text/plain; charset=utf-8');
+header_remove('X-Powered-By');
 
 function _log($msg) {
     file_put_contents('php://stdout', $msg . PHP_EOL);
 }
 
 try {
+    if (!is_dir(UPLOAD_DIR)) {
+        throw new RuntimeException('Uploads directory ' . UPLOAD_DIR .
+                                   ' does not exist!');
+    }
+
+    $upload_dir = realpath(UPLOAD_DIR);
+
+    _log('[INFO] Using uploads directory ' . $upload_dir);
+
     if (!isset($_FILES[FILE_FORM_FIELD])) {
         throw new RuntimeException('Received non-file-upload request');
     }
 
     if ($_FILES[FILE_FORM_FIELD]['size'] > MAX_FILE_SIZE) {
-        throw new RuntimeException('Exceeded filesize limit.');
+        throw new RuntimeException('Exceeded filesize limit');
     }
-
-    // yes, trusting client mime is bad
-    $mime = $_FILES[FILE_FORM_FIELD]['mime'];
-    $ext = explode('/', $mime)[1];
 
     $tmp_file_name = $_FILES[FILE_FORM_FIELD]['tmp_name'];
     $file_sha1 = sha1_file($_FILES[FILE_FORM_FIELD]['tmp_name']);
-    $new_file_name = sprintf('./uploads/%s.%s', $file_sha1, $ext);
+    $new_file_path = $upload_dir . '/' . $file_sha1;
 
-    if (!move_uploaded_file($tmp_file_name, $new_file_name)) {
+    if (!move_uploaded_file($tmp_file_name, $new_file_path)) {
         throw new RuntimeException('Unable to move uploaded file');
     }
 
-    _log('[INFO] Uploaded file now present at ' . realpath($new_file_name));
+    _log('[INFO] Uploaded file now present at ' . $new_file_path);
+    http_response_code(201);
 } catch (RuntimeException $e) {
     _log('[ERROR] ' . $e->getMessage());
+    http_response_code(404);
 }
